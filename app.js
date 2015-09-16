f249161ced09c4b429bd02ca4f2e51bd0a92cf61
pimlu <pimlu@users.noreply.github.com>
2015-09-16 16:38:48 -0400

    comment code

--- app.js
+++ app.js
@@ -4,22 +4,24 @@ var cookieSession = require('cookie-session');
 
 var app = express();
 
+//these app.use calls add middleware- functions that treat/modify requests as they go down routes
 
+//use this to read POST request bodies for us (so we can read forms)
 app.use(bodyParser.urlencoded({
   extended: true
 })); 
-
+//use this so we can store the secret number and how many tries it took them
 app.use(cookieSession({
   name: 'session',
   secret: 'ramdev'
 }));
-
-
+//this makes it so res.render uses jade
 app.set('view engine', 'jade');
 
+//pass our app to the routes.js file so it can add our game page to the route
 require('./routes.js')(app);
 
-
+//listen on port 8080 for incoming requests
 var server = app.listen(8080, function() {
   var host = server.address().address;
   var port = server.address().port;
--- routes.js
+++ routes.js
@@ -6,32 +6,36 @@ function genNum(session) {
 }
 
 module.exports = function(app) {
-
+  //serve static files
   app.use(express.static('public'));
   
+  //respond to get requests with a summary of the game state
   app.get('/', function(req, res) {
     res.render('game', {
       tries: req.session.tries
     });
   });
   
+  //if someone posts, it's because they made a guess with the form
   app.post('/', function(req, res) {
     
+    //increment their tries, and give them a secret number to guess for if there isn't one already
     req.session.tries = (req.session.tries || 0) + 1;
     if(!req.session.number) {
       genNum(req.session);
     }
     
-    var tries = req.session.tries;
-    var guess = +req.body.guess;
-    var actual = req.session.number;
+    var tries = req.session.tries; //stored in session
+    var guess = +req.body.guess; //stored in post body as a string from the textfield
+    var actual = req.session.number; //stored in session
     
-    
+    //if they got it right, reset tries and generate a new number
     if(guess === actual) {
       req.session.tries = 0;
       genNum(req.session);
     }
     
+    //tell jade to respond with a compiled version of the game.jade template with the following parameters
     res.render('game', {
       post: true,
       tries: tries,
