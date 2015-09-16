ce3cd047f4d9b2de461b9eff6b7216a541c0ef8d
pimlu <pimlu@users.noreply.github.com>
2015-09-16 16:46:13 -0400

    validate input

--- routes.js
+++ routes.js
@@ -20,14 +20,21 @@ module.exports = function(app) {
   app.post('/', function(req, res) {
     
     //increment their tries, and give them a secret number to guess for if there isn't one already
-    req.session.tries = (req.session.tries || 0) + 1;
+    var tries = req.session.tries = (req.session.tries || 0) + 1;
+    var guess = +req.body.guess; //stored in post body as a string from the textfield
     if(!req.session.actual) {
       genNum(req.session);
     }
-    
-    var tries = req.session.tries; //stored in session
-    var guess = +req.body.guess; //stored in post body as a string from the textfield
     var actual = req.session.actual; //stored in session
+    
+    //if the guess is invalid, revert their try and just respond as if it's a get
+    if(isNaN(guess) || guess < 1 || guess > 100) {
+      res.render('game', {
+        tries: --req.session.tries,
+        invalid: true
+      });
+      return;
+    }
     
     //if they got it right, reset tries and generate a new number
     if(guess === actual) {
